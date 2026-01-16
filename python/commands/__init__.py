8d3b054786419899aeea401610f9ed111b0fe018
Yanis Srairi <yanis@srairi.fr>
2026-01-16 15:48:43 +0100

    style: Format and organize python/commands/__init__.py
    - Simplify module docstring to single-line format
    - Sort imports alphabetically
    - Convert single quotes to double quotes in __all__
    - Sort __all__ exports alphabetically

--- python/commands/__init__.py
+++ python/commands/__init__.py
@@ -1,19 +1,17 @@
-"""
-KiCAD command implementations package
-"""
+"""KiCAD command implementations package."""
 
-from .project import ProjectCommands
 from .board import BoardCommands
 from .component import ComponentCommands
-from .routing import RoutingCommands
 from .design_rules import DesignRuleCommands
 from .export import ExportCommands
+from .project import ProjectCommands
+from .routing import RoutingCommands
 
 __all__ = [
-    'ProjectCommands',
-    'BoardCommands',
-    'ComponentCommands',
-    'RoutingCommands',
-    'DesignRuleCommands',
-    'ExportCommands'
+    "BoardCommands",
+    "ComponentCommands",
+    "DesignRuleCommands",
+    "ExportCommands",
+    "ProjectCommands",
+    "RoutingCommands",
 ]
